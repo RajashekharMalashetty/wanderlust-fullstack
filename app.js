3f3a46e87495f682a55b2a11574a377bbf420f15
Rajashekhar <malashettyrajashekhar622@gmail.com>
2026-05-08 17:40:59 +0530

    SignUp User - GET

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -75,8 +76,9 @@ app.get("/demouser", async(req, res) => {
     res.send(registeredUser);
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
