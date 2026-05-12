b6f8776947c708068b36438389fcd101c3af04ff
Rajashekhar <malashettyrajashekhar622@gmail.com>
2026-05-12 18:08:36 +0530

    Login after SignUp

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLoggedIn = (req, res, next) => {
- if(!req.isAuthenticated()){
+    console.log(req.user);
+    if(!req.isAuthenticated()){
         req.flash("error", "you must be logged in to create listing!");
         return res.redirect("/login");
     }
