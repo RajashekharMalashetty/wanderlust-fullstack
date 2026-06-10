21d2a9516f7c083eccbe56d7e980d6b4f30b100d
Rajashekhar <malashettyrajashekhar622@gmail.com>
2026-06-10 18:31:17 +0530

    manipulate form

--- routes/listing.js
+++ routes/listing.js
@@ -4,13 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/ '});
 
 router.route("/")
     //Index Route
     .get( wrapAsync(listingController.index))
     // Create Route
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    // .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    .post(upload.single("listing[image]"),(req, res) => {
+        res.send(req.file);
+    });
 
 //New Route
 router.get("/new",isLoggedIn, listingController.renderNewForm); 
